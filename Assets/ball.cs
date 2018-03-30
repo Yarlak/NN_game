103b4fe88e4f70ba481411736f0bb2fe3784c723
Matt <russiello.matt@gmail.com>
2018-03-30 13:49:09 -0400

    Add unity files

---
+++ Assets/ball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+
+Rigidbody r_body;
+
+Vector3 hit_direction;
+
+float up_mult = 7.0f;
+float forward_mult = 20.0f;
+
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.tag == "score")
+		{
+			print("goalllllllll");
+			Application.LoadLevel(0);
+		}
+		
+		
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		
+		if (collision.collider.gameObject.tag == "Player")
+		{
+			hit_direction = transform.position - collision.collider.gameObject.transform.position;
+			hit_direction = hit_direction/hit_direction.magnitude;
+			r_body.velocity = new Vector3 (hit_direction.x * forward_mult, up_mult, hit_direction.z * forward_mult);
+		}
+	}
+
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+		r_body = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/drive.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drive : MonoBehaviour 
+{
+
+	bool stop;
+	int count;
+	
+	float rot_speed;
+	float move_speed;
+	float rot_mult = 2.0f;
+	
+	Collider player_collider;
+	
+	Rigidbody r_body;
+	
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		
+		r_body.velocity = Vector3.zero;
+		r_body.angularVelocity = Vector3.zero;		
+		
+	}
+	
+	
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		player_collider = gameObject.GetComponent<Collider>();
+		r_body = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		move_speed = Input.GetAxis("Vertical");
+		rot_speed = Input.GetAxis("Horizontal");
+		
+		transform.Translate(Vector3.forward * move_speed * 0.2f);
+		transform.Rotate(0.0f, rot_speed * rot_mult, 0.0f);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		if (r_body.velocity.magnitude > 0.0f)
+		{
+			r_body.velocity = Vector3.zero;
+			r_body.angularVelocity = Vector3.zero;
+		}
+		
+	}
+	
+}
